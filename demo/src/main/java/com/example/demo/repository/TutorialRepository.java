6da43d083c7f055288a3351993dfd6bae3e44bfb
unknown <davedalimocon@gmail.com>
2024-01-02 10:40:45 +0800

    tutorial

---
+++ demo/src/main/java/com/example/demo/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.Tutorial;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+
+}
